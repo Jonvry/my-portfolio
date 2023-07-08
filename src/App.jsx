3cfbab2988e11c415482550d429a6e80a50b0bad
jon albert virrey <113243693+Jonvry@users.noreply.github.com>
2023-07-08 14:19:11 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,36 @@ import Project from "./components/project"
 import About from "./components/about"
 import Techstack from "./components/techstack"
 import Skill from "./components/skill"
+import Navbar from "./components/navbar"
 
 export default function App() {
+  // Array to store the IDs
+  const elementIds = ["project", "about", "techStack", "skill"];
   return (
     <main className="main">
+      <Navbar elementIds={elementIds} />
       <div className="name">
         <Name/>
       </div>
-      <div className="project" id="project"> 
+      <div className="project" id={elementIds[0]}> 
         <h3>Project</h3> 
         <Project/>
       </div>
       <small>Design & Build by:
           Jon Albert Virrey<br/> &copy; 2023
         </small>
-      <div className="about" id="about"> 
+      <div className="about" id={elementIds[1]}> 
         <h3>About</h3>
         <About/>
       </div>
-      <div className="techStack" id="techStack"> 
+      <div className="techStack" id={elementIds[2]}> 
         <h3>Tech Stack</h3>
         <Techstack/>
       </div>
-      <div className="skill" id="skill">
+      <div className="skill" id={elementIds[3]}>
         <h3>Skill</h3>
         <Skill/>
        </div>
     </main>
-  )
+  );
 }
-
-
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,52 +1,7 @@
-// import { useState } from "react";
-// import logo from "../assets/logo.svg"
-
-// export default function Navbar() {
-//   const [menu, setMenu] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenu(!menu);
-//   };
-
-// //   const handleClick = () => {
-// //     setMenu(false);
-// //   }
-
-//   return (
-//     <nav className="navBar">
-//         <img className="navLogo" src={logo} alt="" />
-//       <button onClick={toggleMenu}>
-//         {menu ? "CLOSE" : "MENU"}
-//       </button>
-//       <NavList isOpen={menu} />
-//     </nav>
-//   );
-// }
-
-// function NavList({ isOpen }) {
-//   return (
-//     <ul className={`navList ${isOpen ? "open" : ""}`}>
-//       <li className="navLink">
-//         <a href="#about">About</a>
-//       </li>
-//       <li className="navLink">
-//         <a href="#techStack">Tech Stack</a>
-//       </li>
-//       <li className="navLink">
-//         <a href="#skill">Skill</a>
-//       </li>
-//       <li className="navLink">
-//         <a href="#project">Project</a>
-//       </li>
-//     </ul>
-//   );
-// }
-
-
 import { useState } from "react";
 import logo from "../assets/logo.svg";
 
-export default function Navbar() {
+export default function Navbar({ elementIds }) {
   const [menu, setMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -60,44 +15,38 @@ export default function Navbar() {
   return (
     <nav className="navBar">
       <img className="navLogo" src={logo} alt="" />
-      <button onClick={toggleMenu}>{menu ? "CLOSE" : "MENU"}</button>
-      <NavList isOpen={menu} closeMenu={closeMenu} />
+      <button className="navBtn" onClick={toggleMenu}>{menu ? "CLOSE" : "MENU"}</button>
+      <NavList isOpen={menu} closeMenu={closeMenu} elementIds={elementIds} />
     </nav>
   );
 }
 
-function NavList({ isOpen, closeMenu }) {
-  const handleLinkClick = () => {
+function NavList({ isOpen, closeMenu, elementIds }) {
+
+  const handleLinkClick = (id) => {
     closeMenu();
+    scrollToElement(id);
+  };
+
+  const scrollToElement = (id) => {
+    const element = document.getElementById(id);
+      element.scrollIntoView({  block: "center" });
   };
 
   return (
     <ul className={`navList ${isOpen ? "open" : ""}`}>
       <li className="navLink">
-        <a href="#about" 
-           onClick={handleLinkClick}>
-           About
-        </a>
+        <button className="btnLink" onClick={() => handleLinkClick(elementIds[1])}>About</button>
       </li>
       <li className="navLink">
-        <a href="#techStack" 
-           onClick={handleLinkClick}>
-           Tech Stack
-        </a>
+        <button className="btnLink" onClick={() => handleLinkClick(elementIds[2])}>Tech Stack</button>
       </li>
       <li className="navLink">
-        <a href="#skill" 
-           onClick={handleLinkClick}>
-           Skill
-        </a>
+        <button className="btnLink" onClick={() => handleLinkClick(elementIds[3])}>Skill</button>
       </li>
       <li className="navLink">
-        <a href="#project" 
-           onClick={handleLinkClick}>
-           Project
-        </a>
+        <button className="btnLink" onClick={() => handleLinkClick(elementIds[0])}>Project</button>
       </li>
     </ul>
   );
 }
-
